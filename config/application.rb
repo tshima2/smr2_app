9fd272cf82c755ecbdb27f28a30f6a0591e67872
SHIMA Tatsurou <shima.tatsurou@gmail.com>
2022-01-26 14:50:22 +0000

    rspecインストール/設定

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,14 @@ module Smr2App
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-  end
+
+    config.generators.test_framework      = :rspec
+    config.generators.system_tests        = false
+    config.generators.stylesheets         = false
+    config.generators.javascripts         = false
+    config.generators.helper              = false
+    config.generators.helper_specs        = false
+    config.generators.view_specs          = false
+    config.generators.controller_specs    = false
+  end 
 end
