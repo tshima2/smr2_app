58d71adbb27012a4f91c910b0dd7571bd1533a5a
SHIMA Tatsurou <shima.tatsurou@gmail.com>
2022-02-03 03:56:49 +0000

    Site新規作成時の確認機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   get 'sites', to: 'sites#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :sites
+  resources :sites do
+    collection do
+      post :confirm
+    end
+  end
+
 end
