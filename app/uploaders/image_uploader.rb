3985c3203788ff061c5b16313896c335ded6ee68
tshima2 <76857613+tshima2@users.noreply.github.com>
2022-02-18 13:23:43 +0900

    Revert "Revert "fog-awsインストール, production環境のみS3を使うように設定を追加""

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,10 +3,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -14,6 +10,14 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   process resize_to_limit: [400, 300]
+
+  # Choose what kind of storage to use for this uploader:
+  if Rails.env.development? || Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
+  
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
