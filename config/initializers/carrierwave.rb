10574f768d096d055cab3fb2642ff1b923a0c245
SHIMA Tatsurou <shima.tatsurou@gmail.com>
2022-03-11 08:11:11 +0000

    アップロード画像格納先のS3バケットを環境変数で定義するよう変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -19,8 +19,8 @@ CarrierWave.configure do |config|
     #config.fog_public  = true
     config.fog_public  = false
     config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
-    config.fog_directory  = 'smr22-production' #S3のバケット名
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com'
+    config.fog_directory  = ENV["SMR2_APP_BUCKET_NAME"]
+    config.asset_host = ENV["SMR2_APP_BUCKET_HOST"]
   else
     config.storage :file # 開発環境:public/uploades下に保存
     config.enable_processing = false if Rails.env.test? #test:処理をスキップ
